75b188cb942d009b81a2bd10ef1ebc68aaef50ac
git-service-user-at-920203088067 <rodrigotmuniz@gmail.com>
2020-07-17 07:14:20 -0300

    New Version

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ module.exports = {
   decryptToken: require('./functions/decrypt-token'),
   doesUserKeyMatch: require('./functions/does-user-key-match'),
   generatePolicy: require('./functions/generate-policy'),
-  filterUserProfiles: require('./functions/filter-user-profiles')
+  filterUserProfiles: require('./functions/filter-user-profiles'),
+  getResourcePath: require('./functions/get-resource-path')
 }
 
 
